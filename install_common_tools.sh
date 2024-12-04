a794dc89074af0f86da46099e107e931ea4de679
0xNixxy <NicholasHoZY@gmail.com>
2024-12-04 13:16:13 +0800

    Split tool installation to individual component scripts

--- install_common_tools.sh
+++ install_common_tools.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# install_common_tools.sh
-# This script adds the official URLs of my commonly used utilities that are
-# often outdated and not actively maintained in the Ubuntu apt repositories.
+# This script installs the common tools I use for my development environment across systems.
+# As some of the repositories maintained in the Debian and Ubuntu package repositories are very
+# out-of-date, this script adds official repositories to install the latest versions of these tools.
+# This script is targeted at fresh installations. For existing development setups, you may want to
+# invoke the installation scripts for specific tools.
 
-# Assume fresh Linux install, and perform an initial update
-sudo apt update
 
-# Add repo and install: azure CLI
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
-  gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg >/dev/null
-sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
-AZ_DIST=$(lsb_release -cs)
-echo "Types: deb
-URIs: https://packages.microsoft.com/repos/azure-cli/
-Suites: ${AZ_DIST}
-Components: main
-Architectures: $(dpkg --print-architecture)
-Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
-sudo apt-get update &&
-  sudo apt-get install azure-cli -y
+# Update to latest patches
+sudo apt update && sudo apt upgrade
 
-# Add repo and install: gh CLI
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
-  sudo mkdir -p -m 755 /etc/apt/keyrings &&
-  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
-  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-  sudo apt update &&
-  sudo apt install gh -y
+################################################
+# Install my frequently used development tools #
+################################################
+
+# Azure CLI
+install_azure_cli.sh
+
+# GitHub CLI
+install_github_cli.sh
+
+# Rust toolchain
+# install_rust_cli.sh
